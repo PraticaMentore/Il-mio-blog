e338c606bab380e34f99a07f205e5a1c1b08f391
PraticaMentore <francesco_virdis@alice.it>
2025-10-15 11:48:28 +0200

    Initial setup: Quartz with Obsidian symlink

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,17 +8,17 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "PraticaMentore",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "it-IT",
+    baseUrl: "PraticaMentore.it",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
